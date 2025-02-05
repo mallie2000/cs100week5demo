f437575241473beae436b659d11f3e743236d5a2
Muzammil Allie <malli007@xe-01.cs.ucr.edu>
2025-02-05 13:36:45 -0800

    sorry tm8

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,11 +3,12 @@
 #include <string>
 using namespace std;
 
-void greeting(string name){
+string greeting(string name){
     string msg = "Hello " + name;
     return msg;
 }
 int main() {
-    cout << greeting() << endl;
+    string name = "John";
+    cout << greeting(name) << endl;
     return 0;
 }
